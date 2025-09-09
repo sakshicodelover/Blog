8c2337551aac323c19e372c70b76e446cfbc1570
Sakshi Darak <sakshidarak40@gmail.com>
2025-09-09 12:59:42 +0200

    Initial commit

---
+++ comments/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const commentsByPostId = {};
+ app.use(bodyParser.json());
+app.get('/posts/:id/comments',(req,res)=>{
+  res.send(commentsByPostId[req.params.id] || []);
+})
+
+app.post('/posts/:id/comments', (req,res)=>{
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+     let comments = commentsByPostId[req.params.id] || [];
+       comments.push({id : commentId , content})
+       commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
+})
+
+app.listen(4001, ()=>{
+    console.log('listening on 4001')
+})
