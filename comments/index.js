c083d2e7fbc8cb85641c8e836bda982723f245f0
Sakshi Darak <sakshidarak40@gmail.com>
2025-09-10 12:15:05 +0200

    adding event bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {randomBytes} = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 app.use(cors());
 const commentsByPostId = {};
@@ -10,13 +11,20 @@ app.get('/posts/:id/comments',(req,res)=>{
   res.send(commentsByPostId[req.params.id] || []);
 })
 
-app.post('/posts/:id/comments', (req,res)=>{
+app.post('/posts/:id/comments', async(req,res)=>{
+    console.log(req.params.id)
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
      let comments = commentsByPostId[req.params.id] || [];
+       console.log('check',comments)
        comments.push({id : commentId , content})
        commentsByPostId[req.params.id] = comments;
     res.status(201).send(comments);
+})
+
+app.post('/events',(req,res)=>{
+    console.log('event received:', req.body.type);
+    res.send({});
 })
 
 app.listen(4001, ()=>{
